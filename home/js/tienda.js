295923ab32cdda52fe959337c32d263d69e3fb43
MicaelaAraujo <micaela_m_a@hotmail.com>
2022-12-03 18:15:58 -0300

    mi commit

---
+++ home/js/tienda.js
@@ -0,0 +1,77 @@
+console.log("Hola mundo")
+
+
+
+const products = [
+    {
+        name : "Pizza Magic",
+        category : "Pizzas",
+        img :  "america_digital_pizza_vegetariana_faciles_2021-2-1026x570.jpg"
+    },
+    {
+        name : "Pizza con Tomate",
+        category : "Pizzas",
+        img :  "delicia.jpg"
+    },
+    {
+        name : "Pizza de rúcula y tomates",
+        category : "Pizzas",
+        img :  "pizza-napoletana-vegana-3.jpg"
+    },
+    {
+        name : "Pizza de tofu, aceitunas y tomates",
+        category : "Pizzas",
+        img :  "pizza-vegana-con-albahaca-tomates-cherry-y-morron-foto-principal.jpg"
+    },
+    {
+        name : "Pizza de palta y aceitunas",
+        category : "Pizzas",
+        img :  "Pizza-vegetariana-con-masa-de-maiz.png"
+    },
+    {
+        name : "Hamburguesa de lentejas",
+        category : "Hamburguesas",
+        img :  "Hamburguesa 1.jpg"
+    },
+    {
+        name : "Hamburguesa extra de lentejas y lechuga",
+        category : "Hamburguesas",
+        img :  "Hamburguesa 2.jpg"
+    },
+    {
+        name : "Hamburguesa de lechuga y papa",
+        category : "Hamburguesas",
+        img :  "descarga.jpg"
+    },
+    {
+        name : "Hamburguesa de esparrago y zapallo",
+        category : "Hamburguesas",
+        img :  "descarga (1).jpg"
+    },
+    {
+        name : "Hamburguesa de palta",
+        category : "Hamburguesas",
+        img :  "Hamburguesa5.jpg"
+    },
+    {
+        name : "Tomates",
+        category : "Vegetal",
+        img :  "Tomate.jpg"
+    },
+    {
+        name : "Papa",
+        category : "Vegetal",
+        img :  "papa.jpg"
+    },
+    {
+        name : "Ciruela",
+        category : "Fruta",
+        img :  "Ciruela.jpg"
+    },
+    
+
+] 
+
+
+
+	
